fb4266a32e12f0bf82cb9df5107b7f1fd7cf97e6
Lucas Edson <lucasedson654@gmail.com>
2023-08-09 20:14:26 -0300

    undefined

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { $ } from "zx";
+import { argv } from 'node:process';
+
+
+
+if (process.argv.length === 2) {
+    console.error('Por favor, digite a mensagem do Commit!');
+    process.exit(1);
+  }
+else{
+    await $`git add .`
+    await $`git commit -m ${console.log(argv[2])}`
+    await $`git push`
+    await $`echo Código enviado com sucesso!!`
+    // console.log(argv[2])
+    process.exit(1);
+
+}
