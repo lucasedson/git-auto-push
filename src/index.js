fb2e6ff09199753762a5ac78b89bc75ba952afe8
Lucas Edson <lucasedson654@gmail.com>
2023-08-09 22:31:41 -0300

    Script Atualizado com git-auto-push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import { $ } from "zx";
+import { $ } from "zx"; // Importa a bibilioteca zx
 // $.verbose = false
 
-if (process.argv.length === 2) {
-    console.error('Por favor, digite a mensagem do Commit!');
-    process.exit(1);
+
+
+if (process.argv.length === 2) { //Verifica se o usuário digitou alguma mensagem
+    console.error('Por favor, digite a mensagem do Commit!'); //Exibe mensagem de erro
+    process.exit(1); //Finaliza a execução
   }
 else{
-    var message = process.argv[2]     
-    console.log(message)
-    await $`git add .`
-    await $`git commit -m ${message}`
-    await $`git push`
-    console.log("Add, Commit e Push realizado com sucesso!")
+    var message = process.argv[2] //Captura a mensagem 
+    // console.log(message) //Exibe a mensagem
+    await $`git add .` //Adiciona o arquivo no Git
+    await $`git commit -m ${message}` //Comita o arquivo referenciando a mensagem
+    await $`git push` //Envia o arquivo para o Git
+    console.log("Add, Commit e Push realizado com sucesso!") 
     process.exit(1)
 
 }
